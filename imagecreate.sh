c36bbfe83596d661dbf568bdea018a5e61f63027
rern <rernrern@gmail.com>
2020-12-10 15:29:59 +0700

    Update imagecreate.sh

--- imagecreate.sh
+++ imagecreate.sh
@@ -76,7 +76,7 @@ else
 	model=0-1
 fi
 version=$( cat $dirroot/srv/http/data/system/version )
-imagefile=rOS$version_RPi$model.img.xz
+imagefile=rAudio-$version_RPi$model.img.xz
 
 # auto expand root partition
 touch $dirboot/expand
