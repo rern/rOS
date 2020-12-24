f3c747c647edc2175fe3025ef447975812f851c2
rern <rernrern@gmail.com>
2020-12-24 13:05:34 +0700

    Update create-alarm.sh

--- create-alarm.sh
+++ create-alarm.sh
@@ -479,8 +479,8 @@ $nmap
 
 	dialog "${opt[@]}" --ok-label Yes --extra-button --extra-label Rescan --cancel-label No --yesno "\n
 \Z1Found IP address of Raspberry Pi?\Z0
-(Also try ping if router assigned a fixed IP.)
-" 7 38
+(Also try ping for fixed IP.)
+" 8 38
 	ans=$?
 	if [[ $ans == 3 ]]; then
 		scanIP
