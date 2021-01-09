3b21db60b8dbf01fd9175718f8c7d9698779be75
rern <rernrern@gmail.com>
2021-01-09 14:49:49 +0700

    Update create-ros.sh

--- create-ros.sh
+++ create-ros.sh
@@ -165,8 +165,6 @@ for user in $users; do
 done
 # upmpdcli - fix: missing symlink and init RSA key
 if [[ -e /usr/bin/upmpdcli ]]; then
-	# fix - missing symlink
-	[[ ! -e /usr/lib/libjsoncpp.so.22 ]] && ln -s /usr/lib/libjsoncpp.so.{24,22}
 	mpd --no-config &> /dev/null
 	upmpdcli &> /dev/null &
 else
