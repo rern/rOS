e683e1bf311a0b4085453d9b2c24ff20e1b8467f
rern <rernrern@gmail.com>
2021-02-01 13:48:58 +0700

    Update rr.sh

--- rr.sh
+++ rr.sh
@@ -10,12 +10,13 @@ dialog "${optbox[@]}" --infobox "
 sleep 1
 
 cmd=$( dialog "${optbox[@]}" --output-fd 1 --menu "
- \Z1r\Z0Audio image:
+ \Z1r\Z0Audio:
 " 8 0 0 \
 1 'Create' \
 2 'Reset' \
 3 'Image' \
-4 'Write' )
+4 'Write' \
+5 'SSH' )
 
 url=https://github.com/rern/rOS/raw/main
 
@@ -24,4 +25,12 @@ case $cmd in
 	2 ) bash <( wget -qO - $url/reset.sh ) ;;
 	3 ) bash <( wget -qO - $url/imagecreate.sh ) ;;
 	4 ) bash <( wget -qO - $url/imagewrite.sh ) ;;
+	5 ) rpiip=$( dialog "${opt[@]}" --output-fd 1 --inputbox "
+ IP:
+" 0 0 192.168.1. )
+		pw=$( dialog "${opt[@]}" --output-fd 1 --inputbox "
+ Password:
+" 0 0 ros )
+		sed -i "/$rpiip/ d" ~/.ssh/known_hosts
+		sshpass -p "$pw" ssh -t -o StrictHostKeyChecking=no root@$rpiip
 esac
