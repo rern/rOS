bd77bac46eeeba618b6630d4064eab8f3f54f5b9
rern <rernrern@gmail.com>
2021-02-13 14:13:05 +0700

    Update create-ros.sh

--- create-ros.sh
+++ create-ros.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-features=$( cat /boot/features )
-versions=( $( cat /boot/versions ) )
-
-version=${versions[0]}
-revision=${versions[1]}
-uibranch=${versions[2]}
-addonalias=r$version
-
 trap 'rm -f /var/lib/pacman/db.lck; exit' INT
 
+addonalias=r$version
 hwrevision=$( grep Revision /proc/cpuinfo )
 [[ ${hwrevision: -4:1} == 0 ]] && rpi01=1
 
@@ -185,7 +178,7 @@ systemctl enable avahi-daemon cronie devmon@http nginx php-fpm startup
 # data - settings directories
 /srv/http/bash/datareset.sh $version $revision
 # remove files and package cache
-rm -f /boot/{features,versions} /etc/motd /root/create-ros.sh /var/cache/pacman/pkg/*
+rm -f /etc/motd /root/create-ros.sh /var/cache/pacman/pkg/*
 # usb boot - disable sd card polling
 ! df | grep -q /dev/mmcblk && echo 'dtoverlay=sdtweak,poll_once' >> /boot/config.txt
 # expand partition
