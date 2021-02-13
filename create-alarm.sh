059309e8d001e96083fa386111225e12ca50cb96
rern <rernrern@gmail.com>
2021-02-13 13:59:56 +0700

    Update create-alarm.sh

--- create-alarm.sh
+++ create-alarm.sh
@@ -417,7 +417,7 @@ sed -i -e 's/#\(PermitRootLogin \).*/\1yes/
 ' -e 's/#\(PermitEmptyPasswords \).*/\1yes/
 ' $ROOT/etc/ssh/sshd_config
 # ssh - fix: long wait login
-sed -i '/^-.*pam_systemd/ s/^/#/' /etc/pam.d/system-login
+sed -i '/^-.*pam_systemd/ s/^/#/' $ROOT/etc/pam.d/system-login
 
 # set root password
 id=$( awk -F':' '/^root/ {print $3}' $ROOT/etc/shadow )
