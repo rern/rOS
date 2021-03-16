29b08d527b1e106a9ab00cb790f5fe6549b987ad
rern <rernrern@gmail.com>
2021-03-16 19:19:23 +0700

    Update create-ros.sh

--- create-ros.sh
+++ create-ros.sh
@@ -52,8 +52,6 @@ clear -x # needed: fix stdout not scroll
 #----------------------------------------------------------------------------
 banner 'Upgrade kernel and default packages ...'
 
-packages='alsa-utils cronie dosfstools gifsicle hfsprogs i2c-tools imagemagick inetutils jq mpc mpd mpdscribble '
-packages+='nfs-utils nginx-mainline-pushstream nss-mdns ntfs-3g parted php-fpm sshpass sudo udevil wget wiringpi '
 if [[ -e /boot/kernel8.img ]]; then
 	pacman -R --noconfirm linux-aarch64 uboot-raspberrypi
 	pacman -Sy --noconfirm linux-raspberrypi4 raspberrypi-bootloader raspberrypi-bootloader-x raspberrypi-firmware
@@ -65,6 +63,9 @@ pacman -Syu --noconfirm
 [[ $? != 0 ]] && pacman -Syu --noconfirm
 
 banner 'Install packages ...'
+
+packages='alsa-utils cronie dosfstools gifsicle hfsprogs i2c-tools imagemagick inetutils jq mpc mpd mpdscribble '
+packages+='nfs-utils nginx-mainline-pushstream nss-mdns ntfs-3g parted php-fpm sshpass sudo udevil wget wiringpi '
 
 pacman -S --noconfirm --needed $packages $features
 [[ $? != 0 ]] && pacman -S --noconfirm --needed $packages $features
