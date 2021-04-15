9adc9dc685149f1f16a2aa733c2dee9b9e0d6dfa
rern <rernrern@gmail.com>
2021-04-15 21:34:23 +0700

    Update imagereset.sh

--- imagereset.sh
+++ imagereset.sh
@@ -52,8 +52,13 @@ if [[ $select == *' 4 '* ]]; then
 fi
 if [[ $select == *' 5 '* ]] && systemctl -q is-enabled netctl-auto@wlan0; then
 	banner 'Clear Wi-Fi connection ...'
-	systemctl disable netctl-auto@wlan0
-	rm /etc/netctl/* 2> /dev/null
+	readarray -t profiles <<< $( ls -p /etc/netctl | grep -v / )
+	if [[ -n $profiles ]]; then
+		for profile in "${profiles[@]}"; do
+			netctl disable "$profile"
+		done
+		rm /etc/netctl/* 2> /dev/null
+	fi
 fi
 
 wget -q --show-progress https://github.com/archlinuxarm/PKGBUILDs/raw/master/core/pacman-mirrorlist/mirrorlist -O /etc/pacman.d/mirrorlist
