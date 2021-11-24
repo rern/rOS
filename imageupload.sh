48b0ac4fa3c9d90902bf25672ec291a12def1384
rern <rernrern@gmail.com>
2021-11-24 17:32:47 +0700

    Update imageupload.sh

--- imageupload.sh
+++ imageupload.sh
@@ -2,28 +2,28 @@
 
 optbox=( --colors --no-shadow --no-collapse )
 
-user=$( dialog "${optbox[@]}" --output-fd 1 --inputbox "
-User:
-" 0 0 )
-repo=$( dialog "${optbox[@]}" --output-fd 1 --inputbox "
-Repo:
-" 0 0 )
+user=rern
+repo=rAudio-1
 tag=$( dialog "${optbox[@]}" --output-fd 1 --inputbox "
 Tag:
 " 0 0 )
 Token=$( dialog "${optbox[@]}" --output-fd 1 --inputbox "
 Token:
 " 0 0 )
-file=$( dialog "${optbox[@]}" --output-fd 1 --inputbox "
-Image:
-" 0 0 )
-
 id=$( curl -sH "Authorization: token $token" \
 		https://api.github.com/repos/$user/$repo/releases/tags/$tag \
 		| jq .id )
-curl \
-	-H "Authorization: token $token" \
-	-H "Content-Type: $( file -b --mime-type $file )" \
-	--data-binary @"$file" \
-	"https://uploads.github.com/repos/$user/$repo/releases/$id/assets?name=$( basename $file )" \
-	| jq
+		
+imageUpload() {
+	file=$1
+	curl \
+		-H "Authorization: token $token" \
+		-H "Content-Type: $( file -b --mime-type $file )" \
+		--data-binary @"$file" \
+		"https://uploads.github.com/repos/$user/$repo/releases/$id/assets?name=$( basename $file )" \
+		| jq
+}
+
+for rpi in 64bit RPi2 RPi0-1; do
+	imageUpload rAudio-1-$rpi-$tag.img.xz
+done
