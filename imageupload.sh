fe805490b0da6498009e9423111890f4498c7712
rern <rernrern@gmail.com>
2022-01-19 17:24:19 +0700

    Update imageupload.sh

--- imageupload.sh
+++ imageupload.sh
@@ -49,7 +49,7 @@ imageUpload() {
 		-H "Authorization: token $token" \
 		-H "Content-Type: application/x-xz" \
 		--data-binary @"$file" \
-		"https://uploads.github.com/repos/$user/$repo/releases/$id/assets?name=$filename" \
+		https://uploads.github.com/repos/$user/$repo/releases/$id/assets?name=$filename \
 		| jq
 }
 
