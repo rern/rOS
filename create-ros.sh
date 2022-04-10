9d127cb966338bd71ae6036859031e7bb1e5dbae
rern <rernrern@gmail.com>
2022-04-10 13:43:10 +0700

    Update create-ros.sh

--- create-ros.sh
+++ create-ros.sh
@@ -44,7 +44,7 @@ banner 'Upgrade system and default packages ...'
 
 packages='alsaequal alsa-utils audio_spectrum_oled cava cronie cd-discid dosfstools evtest gifsicle hdparm hfsprogs 
 i2c-tools imagemagick inetutils jq mpc mpd nfs-utils nginx-mainline-pushstream nss-mdns 
-parted php-fpm sshpass python-rpi-gpio python-rplcd python-smbus2 raspberrypi-stop-initramfs sudo udevil wget wiringpi'
+parted php-fpm sshpass python-rpi-gpio python-rplcd python-smbus2 python-wheel raspberrypi-stop-initramfs sudo udevil wget wiringpi'
 
 if [[ -e /boot/kernel8.img ]]; then
 	pacman -R --noconfirm linux-aarch64 uboot-raspberrypi
