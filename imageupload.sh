e8733a99d8a83896244d88198cc207041547d491
rern <rernrern@gmail.com>
2022-05-31 19:32:11 +0700

    Update imageupload.sh

--- imageupload.sh
+++ imageupload.sh
@@ -27,9 +27,15 @@ banner() {
 	echo
 }
 
-release=i$( echo ${imgfiles[0]/*-} | cut -d. -f1 )
-
-banner "rAudio Image Files: $release"
+release=$( echo ${imgfiles[0]/*-} | cut -d. -f1 )
+notes='
+| Raspberry Pi        | Image  File | Mirror |
+|:--------------------|:------------|:-------|
+| `4` `3` `2 BCM2837` `Zero 2` | [rAudio-1-64bit-'$release'.img.xz](https://github.com/rern/rAudio-1/releases/download/i'$release'/rAudio-1-64bit-'$release'.img.xz)   | [< file](https://cloud.s-t-franz.de/s/kdFZXN9Na28nfD8/download?path=%2F&files=rAudio-1-64bit-'$release'.img.xz)  |
+| `2 BCM2836`                  | [rAudio-1-RPi2-'$releas'e.img.xz](https://github.com/rern/rAudio-1/releases/download/i'$release'/rAudio-1-RPi2-'$release'.img.xz)     | [< file](https://cloud.s-t-franz.de/s/kdFZXN9Na28nfD8/download?path=%2F&files=rAudio-1-RPi2-'$release'.img.xz)   |
+| *`1` *`Zero`                 | [rAudio-1-RPi0-1-'$release'.img.xz](https://github.com/rern/rAudio-1/releases/download/i'$release'/rAudio-1-RPi0-1-'$release'.img.xz) | [< file](https://cloud.s-t-franz.de/s/kdFZXN9Na28nfD8/download?path=%2F&files=rAudio-1-RPi0-1-'$release'.img.xz) |
+'
+banner "rAudio Image Files: i$release"
 
 cd rAudio-1
-gh release create $release $imgdir/*.img.xz
+gh release create i$release --notes "$notes" $imgdir/*.img.xz
