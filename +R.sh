9cfbc54dc0af00a83bb2acee0e43ddea9b7b2290
rern <rernrern@gmail.com>
2023-06-27 08:45:55 +0700

    Update +R.sh

--- +R.sh
+++ +R.sh
@@ -22,19 +22,20 @@ cmd=$( dialog "${optbox[@]}" --output-fd 1 --menu "
 8 'SSH to RPi' )
 
 url=https://github.com/rern/rOS/raw/main
+urlio=https://github.com/rern/rern.github.io/raw/main
 
 case $cmd in
 	1 ) bash <( curl -sL $url/create.sh );;
 	2 ) bash <( curl -sL $url/reset.sh );;
 	3 ) bash <( curl -sL $url/imagecreate.sh );;
 	4 ) bash <( curl -sL $url/imageupload.sh );;
-	5 ) bash <( curl -L https://github.com/rern/rern.github.io/raw/main/repoupdate.sh );;
-	6 ) bash <( curl -sL https://github.com/rern/rern.github.io/raw/main/distcc-client.sh );;
-	7 ) bash <( curl -sL https://github.com/rern/rern.github.io/raw/main/docker.sh );;
+	5 ) bash <( curl -sL $urlio/repoupdate.sh );;
+	6 ) bash <( curl -sL $urlio/distcc-client.sh );;
+	7 ) bash <( curl -sL $urlio/docker.sh );;
 	8 ) rpiip=$( dialog "${opt[@]}" --output-fd 1 --inputbox "
  IP:
 " 0 0 192.168.1. )
-	sed -i "/$rpiip/ d" ~/.ssh/known_hosts
-	sshpass -p ros ssh -t -o StrictHostKeyChecking=no root@$rpiip
-	;;
+		sed -i "/$rpiip/ d" ~/.ssh/known_hosts
+		sshpass -p ros ssh -t -o StrictHostKeyChecking=no root@$rpiip
+		;;
 esac
