c322b4efa59c54be260ba4aaebcd32e95be649d9
rern <rernrern@gmail.com>
2024-01-04 20:46:14 +0700

    Update create-ros.sh

--- create-ros.sh
+++ create-ros.sh
@@ -41,9 +41,10 @@ clear -x # needed: fix stdout not scroll
 #----------------------------------------------------------------------------
 banner 'Upgrade system and default packages ...'
 
-packages='alsaequal alsa-utils audio_spectrum_oled cava cronie cd-discid dosfstools dtc evtest gifsicle hdparm hfsprogs 
-i2c-tools imagemagick inetutils jq kid3-common mpc mpd nfs-utils nginx-mainline nss-mdns 
-parted php-fpm sshpass python-rpi-gpio python-rplcd python-smbus2 python-websocket-client python-websockets raspberrypi-stop-initramfs sudo udevil wget wiringpi '
+packages='alsaequal alsa-utils audio_spectrum_oled cava cronie cd-discid dosfstools dtc evtest gifsicle 
+hdparm hfsprogs i2c-tools imagemagick inetutils jq kid3-common mpc mpd nfs-utils nginx-mainline nss-mdns 
+parted php-fpm sshpass python-rpi-gpio python-rplcd python-smbus2 python-websocket-client python-websockets 
+raspberrypi-stop-initramfs sudo udevil websocat wget wiringpi '
 
 if [[ -e /boot/kernel8.img ]]; then
 	pacman -R --noconfirm linux-aarch64 uboot-raspberrypi
