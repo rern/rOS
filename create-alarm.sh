f16e2508cbdabcdb9cda9b47b1a371fd1c56d415
rern <rernrern@gmail.com>
2025-02-09 08:39:37 +0700

    Update create-alarm.sh

--- create-alarm.sh
+++ create-alarm.sh
@@ -288,7 +288,7 @@ selectFeatures() { # --checklist <message> <lines exclude checklist box> <0=auto
 	
 	select=" $select "
 	[[ $select == *' 1 '* ]] && list+="$bluealsa"$'\n'  && features+='bluealsa bluez bluez-utils python-dbus python-gobject python-requests '
-	[[ $select == *' 2 '* ]] && list+="$camilla"$'\n'   && features+='camilladsp python-pycamilladsp-plot python-websocket-client '
+	[[ $select == *' 2 '* ]] && list+="$camilla"$'\n'   && features+='camilladsp python-websocket-client '
 	[[ $select == *' 3 '* ]] && list+="$browser"$'\n'   && features+='firefox matchbox-window-manager plymouth-lite-rbp-git upower xf86-video-fbturbo '
 	[[ $select == *' 4 '* ]] && list+="$iwd"$'\n'       && features+='iwd '
 	[[ $select == *' 5 '* ]] && list+="$samba"$'\n'     && features+='samba '
