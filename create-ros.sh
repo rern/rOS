4f1af5f7ed492d0571d9da5bd6f94833c358235a
rern <rernrern@gmail.com>
2025-12-31 11:02:14 +0700

    Update package list in create-ros.sh

--- create-ros.sh
+++ create-ros.sh
@@ -44,11 +44,11 @@ banner 'Upgrade system and default packages ...'
 remove='linux-firmware linux-firmware-amdgpu linux-firmware-broadcom linux-firmware-intel linux-firmware-nvidia linux-firmware-radeon'
 packages='alsaequal alsa-utils cava cronie cd-discid dosfstools dtc evtest gifsicle hdparm hfsprogs 
 i2c-tools imagemagick inetutils iwd jq kid3-common libgpiod mmc-utils mpc mpd mpd_oled nfs-utils nginx-mainline nss-mdns 
-parted php-fpm python-rpi-gpio python-rplcd python-smbus2 python-websocket-client python-websockets sudo udevil websocat wget'
+parted php-fpm python-rpi-gpio python-rplcd python-smbus2 python-websocket-client python-websockets raspberrypi-utils sudo udevil websocat wget'
 
 if [[ -e /boot/kernel8.img ]]; then
 	remove+=' linux-aarch64 uboot-raspberrypi'
-	packages+=' linux-rpi raspberrypi-utils'
+	packages+=' linux-rpi'
 fi
 
 pacman -Rdd --noconfirm $remove
