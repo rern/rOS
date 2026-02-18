bb62a8798ea36f843138882b81a894841360cbc2
rern <rernrern@gmail.com>
2026-02-18 16:44:06 +0700

    Update imageupload.sh

--- imageupload.sh
+++ imageupload.sh
@@ -92,7 +92,7 @@ for model in 64bit 32bit Legacy; do
 	"extract_size": '${size_xz_img/* }',
 	"image_download_size": '${size_xz_img/ *}',
 	"image_download_sha256": "'$sha256'",
-	"icon": "https://github.com/rern/rAudio/raw/refs/heads/main/srv/http/assets/img/icon'$model'.png",
+	"icon": "https://raw.githubusercontent.com/rern/_assets/refs/heads/master/rpi-imager/icon'$model'.png",
 	"website": "https://github.com/rern/rAudio"
 }'
 done
