9092a382a634e8901f31b8b37b5d7bee52925711
rern <rernrern@gmail.com>
2026-02-20 06:16:30 +0700

    Update +R.sh

--- +R.sh
+++ +R.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+[[ ! -e common.sh ]] && echo '
+banner() {
+	local cols col_s
+	cols=$( tput cols )
+	col_s=%${cols}s
+	text=$( printf "  $( echo $@ )$col_s" )
+    printf "\n\e[44m$col_s\n${text:0:$cols}\n$col_s\e[0m\n"
+}
+errorExit() {
+	banner E r r o r
+	echo -e "\n\e[41m ! \e[0m $@"
+	exit
+}
+' > common.sh
+
 optbox=( --colors --no-shadow --no-collapse )
 
 dialog "${optbox[@]}" --infobox "
