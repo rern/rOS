46944a431842a5ca0b6a698c4e1aca38f16c9c71
rern <rernrern@gmail.com>
2026-02-21 13:08:03 +0700

    Update +R.sh

--- +R.sh
+++ +R.sh
@@ -4,18 +4,13 @@
 
 . common.sh
 
-runScript() {
-	[[ $1 = [dr]* ]] && repo=rOS || repo=rern.github.io
-	bash <( curl -sL https://github.com/rern/$repo/raw/main/$1 )
-}
-
 dialog $opt_info "
 
 
                         \Z1r\Z0Audio
 " 9 58
 sleep 1
-cmd=$( dialog "${optbox[@]}" --output-fd 1 --menu "
+cmd=$( dialog $opt_menu "
  \Z1r\Z0Audio:
 " 8 0 0 \
 1 'Create rAudio' \
@@ -26,18 +21,15 @@ cmd=$( dialog "${optbox[@]}" --output-fd 1 --menu "
 6 'Distcc client' \
 7 'Docker' \
 8 'SSH to RPi' )
-case $cmd in
-	1 ) runScript create;;
-	2 ) runScript reset;;
-	3 ) runScript imagecreate;;
-	4 ) runScript imageupload;;
-	5 ) runScript repoupdate;;
-	6 ) runScript distcc-client;;
-	7 ) runScript docker;;
-	8 ) rpiip=$( dialog "${opt[@]}" --output-fd 1 --inputbox "
+names=( '' create reset imagecreate imageupload repoupdate distcc-client docker )
+name=${names[$cmd]}
+if [[ $name ]]; then
+	(( $cmd > 4 )) && repo=rOS || repo=rern.github.io
+	bash <( curl -sL https://github.com/rern/$repo/raw/main/$name )
+else # 8
+	rpiip=$( dialog $opt_input "
  IP:
 " 0 0 192.168.1. )
-		sed -i "/$rpiip/ d" ~/.ssh/known_hosts
-		sshpass -p ros ssh -t -o StrictHostKeyChecking=no root@$rpiip
-		;;
-esac
+	sed -i "/$rpiip/ d" ~/.ssh/known_hosts
+	sshpass -p ros ssh -t -o StrictHostKeyChecking=no root@$rpiip
+fi
