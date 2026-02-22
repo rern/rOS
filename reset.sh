70394788e5c2c1046cd71064f2274cf2a370e569
rern <rernrern@gmail.com>
2026-02-22 13:26:58 +0700

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -16,5 +16,7 @@ subip=${routerip%.*}.
 rpiip=$( dialog $opt_input "
  \Z1Raspberry Pi IP:\Z0
 " 0 0 $subip )
+[[ ! $rpiip ]] && exit
+#----------------------------------------------------------------------------
 sed -i "/$rpiip/ d" ~/.ssh/known_hosts
 sshpass -p ros ssh -t -o StrictHostKeyChecking=no root@$rpiip 'bash <( curl -sL https://github.com/rern/rOS/raw/main/imagereset.sh )'
