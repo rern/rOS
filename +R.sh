737ff645c87161a821593934921211069104ecfc
rern <rernrern@gmail.com>
2026-02-24 20:22:39 +0700

    Update +R.sh

--- +R.sh
+++ +R.sh
@@ -5,29 +5,32 @@ file_common=/usr/local/bin/common.sh
 
 . common.sh
 
+#........................
 dialog $opt_info "
 
 
                         \Z1r\ZnAudio
 " 9 58
 sleep 1
+#........................
 cmd=$( dialog $opt_menu '
-Tasks:
+\Z1Tasks:\Zn
 ' 8 0 0 \
-1 'OS - Create' \
-2 'OS - Reset to default' \
-3 'Image - Create' \
-4 'Images - Upload' \
-5 'Repository - Update packages' \
-6 'Distcc client' \
-7 'Docker' \
-8 'SSH to RPi' )
+	1 'OS - Create' \
+	2 'OS - Reset to default' \
+	3 'Image - Create' \
+	4 'Images - Upload' \
+	5 'Repository - Update packages' \
+	6 'Distcc client' \
+	7 'Docker' \
+	8 'SSH to RPi' )
 names=( '' create reset image-create image-upload repoupdate distcc-client docker )
 name=${names[$cmd]}
 if [[ $name ]]; then
 	(( $cmd < 5 )) && repo=rOS || repo=rern.github.io
 	bash <( curl -sL "https://github.com/rern/$repo/raw/main/$name.sh" )
 else # 8
+#........................
 	rpiip=$( dialog $opt_input "
  IP:
 " 0 0 192.168.1. )
