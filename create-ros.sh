3533f84ce2635bba73fe3154013d98a92e7d9ebd
rern <rernrern@gmail.com>
2026-04-06 19:41:33 +0700

    u

--- create-ros.sh
+++ create-ros.sh
@@ -129,7 +129,7 @@ if [[ -e /bin/firefox ]]; then
 	echo MOZ_USE_XINPUT2 DEFAULT=1 >> /etc/security/pam_env.conf # fix touch scroll
 	chmod 775 /etc/X11/xorg.conf.d                               # fix permission for rotate file
 	mv /usr/share/X11/xorg.conf.d/{10,45}-evdev.conf             # reorder
-	firefox --headless &> /dev/null                              # init .config/mozilla/firefox/...
+	firefox --headless &> /dev/null &                            # init .config/mozilla/firefox/...
 	for i in {0..5}; do
 		sleep 1
 		[[ $( find /root -type d -path '/root/*mozilla' ) ]] && pkill firefox && break
