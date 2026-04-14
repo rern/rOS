868fc3593f64fd9977ba2ba2dca845eaa5e9f85b
rern <rernrern@gmail.com>
2026-04-14 14:53:50 +0700

    u

--- image-create.sh
+++ image-create.sh
@@ -10,21 +10,22 @@ package.required bsdtar dialog
 
 shrink() {
 	bar "Shrink Pass #$1 ..."
-	read b_used b_size b_count < <( tune2fs -l $PART_R | awk '
-										/^Block count/ { count=$NF }
-										/^Free blocks/ { free=$NF }
-										/^Block size/  { size=$NF }
-											END { print ( count - free ), size, count }' )
-	b_target=$(( ( b_used * 105 ) / 100 ))
-	s_size=$( blockdev --getss $DEV )
-	s_end=$(( ( b_target * b_size ) / s_size ))
-	if (( $(( b_count - b_target )) < 1024 )); then
+	read blk_used blk_size blk_count < <( tune2fs -l $PART_R | awk '
+											/^Block count/ { count=$NF }
+											/^Free blocks/ { free=$NF }
+											/^Block size/  { size=$NF }
+												END { print ( count - free ), size, count }' )
+	blk_target=$(( ( blk_used * 105 ) / 100 ))
+	byte_target=$(( blk_target * blk_size ))
+	if (( $(( blk_count - blk_target )) < 1024 )); then
 		bar Almost at minimum size already.
 	else
-		b_new=$(( ( b_target * b_size ) / 1024 ))
-		resize2fs -fp $PART_R ${b_new}K
-		s_start=$( cat /sys/class/block/${PART_R/*\/}/start )
-		sfdisk "$DEV" -N ${PART_R: -1} --force <<< "$s_start, $s_end"
+		blk_new=$(( ( blk_target * blk_size ) / 1024 ))
+		resize2fs -fp $PART_R ${blk_new}K
+		sect_start=$( cat /sys/class/block/${PART_R/*\/}/start )
+		sect_size=$( blockdev --getss $DEV )
+		sect_end=$(( byte_target / sect_size ))
+		sfdisk "$DEV" -N ${PART_R: -1} --force <<< "$sect_start, $sect_end"
 	fi
 }
 
@@ -36,13 +37,11 @@ BR.mount
 file_r1=ROOT/srv/http/data/addons/r1
 [[ ! -e $file_r1 ]] && dialog.error_exit "SD card is not rAudio: \Z1$DEV\Zn"
 #------------------------------------------------------------------------------
-if [[ -e BOOT/kernel8.img ]]; then
-	model=64bit
-elif [[ -e BOOT/kernel7.img ]]; then
-	model=32bit
-else # BOOT/kernel.img
-	model=Legacy
-fi
+case $( basename $( ls /boot/kernel*.img ) .img ) in
+	kernel8 ) model=64bit;;
+	kernel7 ) model=32bit;;
+	* )       model=Legacy;;
+esac
 release=$( < $file_r1 )
 #............................
 file_img=$( dialog.input 'Image filename:' rAudio-$model-$release.img.xz )
@@ -61,8 +60,10 @@ shrink 2
 #............................
 banner Compressed to image file ...
 bar $file_img
+blk_size=512
+blk_end=$(( byte_target / blk_size ))
 threads=$(( $( nproc ) - 2 ))
-dd if=$DEV bs=512 iflag=fullblock count=$s_end | nice -n 10 xz -v -T $threads > "$file_img"
+dd if=$DEV iflag=fullblock bs=$blk_size count=$blk_end | nice -n 10 xz -v -T $threads > "$file_img"
 size=$( xz -l --robot $file_img | awk '/^file/ {printf "%.2f MB <<< %.2f GB", $4/10^6, $5/10^9}' )
 bar "Image file created:
 $file_img
