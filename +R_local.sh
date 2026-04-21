e08dce33b82cfbf87940c9098ea1e3954718dca9
rern <rernrern@gmail.com>
2026-04-21 10:29:42 +0700

    u

--- +R_local.sh
+++ +R_local.sh
@@ -2,9 +2,6 @@
 
 BRANCH=${1:-main}
 . <( curl -sL https://github.com/rern/rOS/raw/$BRANCH/+R.sh )
-
-
-
 #..........................................................
 # chmod +x +R.sh
 # echo 'export PATH="/root:$PATH"' >> .bashrc
