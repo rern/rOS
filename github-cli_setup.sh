17e50692a8aab74bc55b24e07252002e279d2020
rern <rernrern@gmail.com>
2026-04-23 11:07:27 +0700

    u

--- github-cli_setup.sh
+++ github-cli_setup.sh
@@ -1,25 +1,36 @@
 #/bin/bash
 
-bar 'gh auth login:
+color() {
+	[[ ${1:0:1} == '(' ]] && c=7 || c=10
+	echo "\e[38;5;${c}m$1\e[0m"
+}
+tick=$( color ✓ )
+dot=$( color » )
+code=$( color CODE-CODE )
+bar "gh auth login:
+
 ? What account do you want to log into?
-	» GitHub.com
+	$tick GitHub.com
 ? What is your preferred protocol for Git operations on this host?
-	» HTTPS
+    $tick HTTPS
 ? Authenticate Git with your GitHub credentials?
-	» Y
+    $tick Y
 ? How would you like to authenticate GitHub CLI?
-	» Login with a web browser
-! First copy your one-time code: CODE-CODE
-	» Copy the code
+    $tick Login with a web browser
+! First copy your one-time code: $code
+    $dot Copy $code
 Press [Enter] to open https://github.com/login/device in your browser...
-	» Do not 'Press Enter' yet
-» Manually open browser: https://github.com/login/device
-	» Select user (login if not already)
-	» Paste the copied code (from terminal) » `Authorize`
-	» Enter verifying code (by Authenticator app - if prompt)
+    $dot Do not 'Press Enter' yet
+$dot Manually open browser: https://github.com/login/device
+    $dot Select user $( color '(login if not already)' )
+    $dot Paste the copied code $( color '(from terminal)' ) » Authorize
+    $dot Enter verifying code $( color '(by Authenticator app - if prompt)' )
 » Press [Enter] (Ignore warning: Running Firefox as root ...)
-✓ Authentication complete.
-'
+    $dot Ignore error message
+
+$tick Authentication complete.
+"
+
 gh auth login
 email=$( dialog.input 'GitHub email:' )
 user=$( dialog.input 'GitHub username:' )
