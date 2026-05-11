9e156b42948502aa5414b1379c5bcfed8cbaded2
rern <rernrern@gmail.com>
2026-05-11 18:06:59 +0700

    u

--- ventoy-setup.sh
+++ ventoy-setup.sh
@@ -5,9 +5,6 @@ echo -e "\n\e[44m  \e[0m V e n t o y - A New Bootable USB Solution\n"
 url=https://github.com/ventoy/Ventoy/releases
 latest=$( curl -sL -o /dev/null -w %{url_effective} $url/latest | awk -F/ '{print $NF}' )
 name_ver=ventoy-${latest:1}
-
-echo Latest version: $name_ver
-
 curl -sL $url/download/$latest/$name_ver-linux.tar.gz | bsdtar xf -
 cd $name_ver
 ./VentoyWeb.sh
