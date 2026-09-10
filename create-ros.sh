35cae300c7e8b4b6408ee6e6feb6f2ad2f0ceca2
rern <rernrern@gmail.com>
2026-09-10 07:06:53 +0700

    Update create-ros.sh

--- create-ros.sh
+++ create-ros.sh
@@ -98,7 +98,6 @@ for repo in rAudio rAudio-assets rOS; do
 		| grep '/.*/'
 done
 find / -maxdepth 1 -type f -delete
-mv '/srv/http/data/webradio/Radio Paradise - Beyond'{,...}
 # default dirs
 . /srv/http/bash/settings/system-datadefault.sh
 [[ -e /boot/kernel8.img ]] && arch=aarch64 || arch=armv7h
@@ -112,6 +111,7 @@ cat << EOF > $dirmpd/counts
 , "webradio"  : $( find $dirwebradio/ -maxdepth 1 -type f | wc -l )
 }
 EOF
+mv "$dirwebradio/Radio Paradise - Beyond"{,...}
 # bluetooth
 if [[ -e /bin/bluetoothctl ]]; then
 	sed -i 's/#*\(AutoEnable=\).*/\1true/' /etc/bluetooth/main.conf
